srcagency/active-document

commit 54e04556f218de9ce8905398c7b9aad82a3b0956

0.1.0

src/index.js
@@ -1,0 +1,82 @@
+'use strict';
+
+var getAllPropertyNames = require('property-names');
+var extend = require('extend');
+var mutator = require('mutator-name');
+var debug = require('debug')('active-document');
+
+var activeDocument = module.exports = {
+	decorate: function( ctor ){
+		debug('%s.decorate', ctor.name);
+
+		extend(ctor, {
+			attributeNames: [],
+			addAttribute: addAttribute,
+
+			toJSON: ctor.toJSON || toJSON,
+			fromJSON: ctor.fromJSON || fromJSON,
+		});
+
+		getAllPropertyNames(ctor.prototype)
+			.forEach(function( attr ){
+				if (attr[0] !== '_' && (mutator(attr) || typeof ctor.prototype[attr] !== 'function'))
+					ctor.addAttribute(mutator.nameFrom(attr) || attr);
+			});
+
+		extend(ctor.prototype, {
+			toJSON: selfToJSON,
+		});
+	},
+
+	init: function( attributes ){
+		attributesConfig.attributes.value = attributes || {};
+		Object.defineProperty(this, 'attributes', attributesConfig.attributes);
+	},
+};
+
+var attributesConfig = {
+	attributes: {
+		writable: false,
+		enumerable: false,
+		configurable: false,
+	},
+};
+
+function fromJSON( json ){
+	debug('%s.fromJSON %j', this.name, json);
+
+	attributesConfig.attributes.value = json;
+	return Object.create(this.prototype, attributesConfig);
+}
+
+function toJSON( obj ){
+	return obj.toJSON();
+}
+
+function selfToJSON(){
+	return extend(true, {}, this.attributes);
+}
+
+function addAttribute( attr ){
+	if (!attr)
+		throw new Error('Cannot add attribute without a name');
+
+	if (~this.attributeNames.indexOf(attr))
+		return;
+
+	debug('%s.addAttribute: adding %s', this.name, attr);
+
+	this.attributeNames.push(attr);
+
+	Object.defineProperty(this.prototype, attr, {
+		writeable: false,
+		enumerable: true,
+		configurable: false,
+		get: this.prototype[mutator.getterFrom(attr)] || function(){
+			return this.attributes[attr];
+		},
+		set: this.prototype[mutator.setterFrom(attr)] || function( value ){
+			return this.attributes[attr] = value;
+		},
+	});
+}

index.js
@@ -1,115 +1,0 @@
-'use strict';
-
-var getAllPropertyNames = require('property-names');
-var extend = require('extend');
-var mutator = require('mutator-name');
-var debug = require('debug')('active-document');
-
-var activeDocument = module.exports = {
-
-	decorate: function( ctor ) {
-		debug('%s.decorate', ctor.name);
-
-		extend(ctor, {
-			attributeNames: [],
-			addAttribute: addAttribute,
-
-			toJSON: ctor.toJSON || toJSON,
-			fromJSON: ctor.fromJSON || fromJSON,
-		});
-
-		extend(ctor.prototype, {
-			get: getter,
-			set: setter,
-
-			toJSON: ctor.prototype.toJSON || protoToJSON,
-		});
-
-		getAllPropertyNames(ctor.prototype)
-			.forEach(function( name ){
-				if (typeof ctor.prototype[name] !== 'function' && name[0] !== '_')
-					ctor.addAttribute(name);
-
-				if (mutator(name))
-					ctor.addAttribute(mutator.nameFrom(name));
-			});
-	},
-
-	init: function( attributes ) {
-		if (debug.enabled)
-			debug('%s.init with attribute keys %o', this.constructor.name, attributes && Object.keys(attributes));
-
-		this.attributes = attributes || {};
-	},
-
-};
-
-// ctor methods
-
-var toJSON = function( obj ) {
-	return obj.toJSON();
-};
-
-var fromJSON = function( json, byReference ) {
-	debug('%s.fromJSON', this.name);
-	return new this(byReference ? json : extend({}, json));
-};
-
-var addAttribute = function( attributeName ) {
-	if (!attributeName)
-		throw new Error('Cannot add attribute without a name');
-
-	if (~this.attributeNames.indexOf(attributeName))
-		return;
-
-	debug('%s.addAttribute: adding %s', this.name, attributeName);
-
-	this.attributeNames.push(attributeName);
-	ensureAttributeFunc(this, attributeName);
-	addAttributeProp(this, attributeName);
-};
-
-// proto methods
-
-var protoToJSON = function() {
-	return extend({}, this.attributes);
-};
-
-var getter = function( attr ) {
-	return this.attributes[attr];
-};
-
-var setter = function( attr, value ) {
-	return this.attributes[attr] = value;
-};
-
-// helpers
-
-var ensureAttributeFunc = function( ctor, attrName ) {
-	var AttrName = ucfirst(attrName);
-
-	if (!ctor.prototype['get'+AttrName])
-		ctor.prototype['get'+AttrName] = function() { return this.get(attrName); };
-
-	if (!ctor.prototype['set'+AttrName])
-		ctor.prototype['set'+AttrName] = function( value ) { return this.set(attrName, value); };
-};
-
-var addAttributeProp = function( ctor, attrName ) {
-	var AttrName = ucfirst(attrName);
-	var AttrName = ucfirst(attrName);
-	var getter = ctor.prototype['get'+AttrName];
-	var setter = ctor.prototype['set'+AttrName];
-	var config = {
-		writeable: false,
-		enumerable: true,
-	};
-
-	if (getter)
-		config.get = getter;
-
-	if (setter)
-		config.set = setter;
-
-	Object.defineProperty(ctor.prototype, attrName, config);
-};

test/index.js
@@ -1,0 +1,71 @@
+'use strict';
+
+var activeDocument = require('../');
+var test = require('tape');
+var extend = require('extend');
+
+test(function( t ){
+	var year = (new Date()).getFullYear();
+
+	function Person(){
+		activeDocument.init.apply(this);
+
+		this.born = year;
+	};
+
+	extend(Person.prototype, {
+		firstName: null,
+		lastName: null,
+		born: null,
+		unused: null,
+		bff: null,
+		_secret: null,
+
+		marry: function(){},
+
+		setLastName: function( name ){
+			this.attributes.lastName = name.substr(0, 1).toUpperCase() + name.substr(1);
+		},
+
+		getName: function(){
+			return this.firstName + ' ' + this.lastName;
+		},
+	});
+
+	activeDocument.decorate(Person);
+
+	t.deepEqual(Person.attributeNames, [ 'firstName', 'lastName', 'born', 'unused', 'bff', 'name' ]);
+
+	var a = new Person();
+
+	var friend = new Person();
+	friend.firstName = 'Jacob';
+
+	var dude = Person.fromJSON({ lastName: 'Smith' });
+	dude.bff = friend;
+
+	t.ok(a.setLastName);
+	t.notOk(a.getLastName);
+	t.equal(typeof a.marry, 'function');
+
+	t.equal(friend.firstName, 'Jacob');
+
+	t.equal(a.firstName, undefined);
+	t.equal(a.firstName = 'Sara', 'Sara');
+	t.equal(a.firstName, 'Sara');
+
+	t.equal(a.lastName, undefined);
+	t.equal(a.lastName = 'jones', 'jones');
+	t.equal(a.lastName, 'Jones');
+
+	t.equal(a.name, 'Sara Jones');
+
+	t.deepEqual(a.toJSON(), { firstName: 'Sara', lastName: 'Jones', born: year });
+	t.deepEqual(a.attributes, a.toJSON());
+
+	t.deepEqual(dude.toJSON(), { bff: dude.bff, lastName: 'Smith' });
+
+	t.equal(JSON.stringify(dude.toJSON()), '{"lastName":"Smith","bff":{"born":2014,"firstName":"Jacob"}}');
+
+	t.end();
+});

test.js
@@ -1,58 +1,0 @@
-/* jslint node: true */
-'use strict';
-
-var activeDocument = require('./');
-var test = require('tap').test;
-var extend = require('extend');
-
-test('active-document', function ( t ) {
-	var Person = function ( attributes ) {
-		activeDocument.init.call(this, attributes);
-	};
-
-	extend(Person.prototype, {
-		firstName: null,
-		lastName: null,
-		born: null,
-		unused: null,
-
-		setLastName: function ( name ) {
-			return this.set('lastName', name+'!');
-		},
-
-		getName: function () {
-			return this.firstName+' '+this.lastName;
-		},
-
-		getBorn: function() {
-			return 'nonsense';
-		},
-	});
-
-	activeDocument.decorate(Person);
-
-	var me = new Person();
-
-	var friend = new Person();
-	friend.firstName = 'Jacob';
-
-	t.equal(me.firstName, undefined);
-	t.equal(me.firstName = 'Thomas', 'Thomas');
-	t.equal(me.firstName, 'Thomas');
-
-	t.equal(me.getLastName(), undefined);
-	t.equal(me.setLastName('Jensen'), 'Jensen!');
-	t.equal(me.getLastName(), 'Jensen!');
-
-	t.equal(me.name, 'Thomas Jensen!');
-
-	t.equal(me.get('born'), undefined);
-	t.equal(me.set('born', 1991), 1991);
-	t.equal(me.get('born'), 1991);
-
-	t.equal(friend.firstName, 'Jacob');
-
-	t.deepEqual(me.attributes, { firstName: 'Thomas', lastName: 'Jensen!', 'born': 1991 });
-
-	t.end();
-});

commit 31952842318a8b28eae44249dfc022d620cebcc2

fix bug introduced in 54e0455 - overriding toJSON

src/index.js
@@ -24,7 +24,7 @@ var activeDocument = module.exports = {
 			});
 
 		extend(ctor.prototype, {
-			toJSON: selfToJSON,
+			toJSON: ctor.prototype.toJSON || selfToJSON,
 		});
 	},
 